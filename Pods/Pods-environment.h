fa96ebe3de2b6871d7bd3934ffd2fc867e269478
Stephen Caudill <scaudill@gmail.com>
2013-11-02 13:33:01 -0600

    Add cocoapods (and Funcussion)

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Funcussion
+#define COCOAPODS_POD_AVAILABLE_Funcussion
+#define COCOAPODS_VERSION_MAJOR_Funcussion 0
+#define COCOAPODS_VERSION_MINOR_Funcussion 1
+#define COCOAPODS_VERSION_PATCH_Funcussion 1
+
