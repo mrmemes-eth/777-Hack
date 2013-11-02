2ae1620b7c3e39453b08b4fbda9581a403553906
Stephen Caudill <scaudill@gmail.com>
2013-11-02 15:27:21 -0600

    Add board background

--- App/BoardScene.m
+++ App/BoardScene.m
@@ -1,5 +1,29 @@
 #import "BoardScene.h"
 
+@interface BoardScene() {
+  SKSpriteNode *_boardNode;
+}
+-(SKSpriteNode*)boardNode;
+@end
+
 @implementation BoardScene
+
+-(id)initWithSize:(CGSize)size {
+  if (self = [super initWithSize:size]) {
+    [self setScaleMode:SKSceneScaleModeAspectFit];
+    [self setBackgroundColor:[SKColor redColor]];
+    [self addChild:self.boardNode];
+  }
+  return self;
+}
+
+-(SKSpriteNode*)boardNode {
+  if (!_boardNode) {
+    SKTexture *texture = [SKTexture textureWithCGImage:[[UIImage imageNamed:@"BoardBG"] CGImage]];
+    _boardNode = [SKSpriteNode spriteNodeWithTexture:texture size:CGSizeMake(1024, 768)];
+    [_boardNode setPosition:CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame))];
+  }
+  return _boardNode;
+}
 
 @end
