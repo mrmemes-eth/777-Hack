a21fb2b63b1b298d151f840c28952f251798ac05
Stephen Caudill <scaudill@gmail.com>
2013-11-14 22:01:57 -0700

    Refactor tests

--- Units/BoardTest.m
+++ Units/BoardTest.m
@@ -36,51 +36,42 @@
 
 -(void)testMoveToUnoccupied {
   Board *board = [Board new];
-  [board addHacker:[Hacker new] atSector:SectorMake(1, 1)];
-  SpriteSectorNode *player =[board.nodes detect:^BOOL(SpriteSectorNode *node) {
-    return [node.name isEqualToString:@"hacker"];
-  }];
+  Hacker *hacker = [Hacker new];
+  [board addHacker:hacker atSector:SectorMake(1, 1)];
   Sector newSector;
-  newSector = [board newSectorForNode:player inDirection:UISwipeGestureRecognizerDirectionRight];
+  newSector = [board newSectorForNode:hacker inDirection:UISwipeGestureRecognizerDirectionRight];
   XCTAssertEqual(newSector, SectorMake(1, 2), @"");
-  newSector = [board newSectorForNode:player inDirection:UISwipeGestureRecognizerDirectionLeft];
+  newSector = [board newSectorForNode:hacker inDirection:UISwipeGestureRecognizerDirectionLeft];
   XCTAssertEqual(newSector, SectorMake(1, 0), @"");
-  newSector = [board newSectorForNode:player inDirection:UISwipeGestureRecognizerDirectionUp];
+  newSector = [board newSectorForNode:hacker inDirection:UISwipeGestureRecognizerDirectionUp];
   XCTAssertEqual(newSector, SectorMake(2, 1), @"");
-   newSector = [board newSectorForNode:player inDirection:UISwipeGestureRecognizerDirectionDown];
+   newSector = [board newSectorForNode:hacker inDirection:UISwipeGestureRecognizerDirectionDown];
   XCTAssertEqual(newSector, SectorMake(0, 1), @"");
 }
 
 -(void)testMoveToOccupied {
   Board *board = [Board new];
-  [board addHacker:[Hacker new] atSector:SectorMake(1, 1)];
-  SpriteSectorNode *player =[board.nodes detect:^BOOL(SpriteSectorNode *node) {
-    return [node.name isEqualToString:@"hacker"];
-  }];
+  Hacker *hacker = [Hacker new];
+  [board addHacker:hacker atSector:SectorMake(1, 1)];
   [board addDataNodeAtSector:SectorMake(1, 2)];
-  Sector newSector = [board newSectorForNode:player
+  Sector newSector = [board newSectorForNode:hacker
                                  inDirection:UISwipeGestureRecognizerDirectionRight];
-  XCTAssertEqual(newSector, player.sector, @"");
+  XCTAssertEqual(newSector, hacker.sector, @"");
 }
 
 -(void)testMoveOutOfBounds {
   Board *board = [Board new];
-  [board addHacker:[Hacker new] atSector:SectorMake(1, 5)];
-  SpriteSectorNode *player =[board.nodes detect:^BOOL(SpriteSectorNode *node) {
-    return [node.name isEqualToString:@"hacker"];
-  }];
-  Sector newSector = [board newSectorForNode:player
+  Hacker *hacker = [Hacker new];
+  [board addHacker:hacker atSector:SectorMake(1, 5)];
+  Sector newSector = [board newSectorForNode:hacker
                                  inDirection:UISwipeGestureRecognizerDirectionRight];
-  XCTAssertEqual(newSector, player.sector, @"");
+  XCTAssertEqual(newSector, hacker.sector, @"");
   
   board = [Board new];
-  [board addHacker:[Hacker new] atSector:SectorMake(0, 0)];
-  player =[board.nodes detect:^BOOL(SpriteSectorNode *node) {
-    return [node.name isEqualToString:@"hacker"];
-  }];
-  newSector = [board newSectorForNode:player
-                                 inDirection:UISwipeGestureRecognizerDirectionLeft];
-  XCTAssertEqual(newSector, player.sector, @"");
+  [board addHacker:hacker atSector:SectorMake(0, 0)];
+  newSector = [board newSectorForNode:hacker
+                          inDirection:UISwipeGestureRecognizerDirectionLeft];
+  XCTAssertEqual(newSector, hacker.sector, @"");
 }
 
 -(void)testMoveOntoWarpZone {
@@ -98,7 +89,7 @@
   [board addHacker:hacker atSector:SectorMake(0, 0)];
   [board.nodes addObject:warp];
   newSector = [board newSectorForNode:hacker
-                                 inDirection:UISwipeGestureRecognizerDirectionRight];
+                          inDirection:UISwipeGestureRecognizerDirectionRight];
   XCTAssertEqual(newSector, newSector, @"");
 }
 
